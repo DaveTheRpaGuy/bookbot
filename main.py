65beba4f0189fdac84a61163d35a51650c5fec08
davetherpaguy <dave@davetherpaguy.com>
2025-08-19 18:54:13 -0400

    added arguments and tested successfully

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import sys
+
 from stats import get_num_words, count_characters, sort_character_counts
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    text = get_book_text(file_path)
     num_words = get_num_words(text)
     char_counts = count_characters(text)
     sorted_char_counts = sort_character_counts(char_counts)
@@ -17,7 +23,7 @@ def print_report(num_words, sorted_char_counts):
     print("----------- Word Count ----------")
     
     print(f"Found {num_words} total words")
-    
+
     print("--------- Character Count -------")
 
     for line in sorted_char_counts:
